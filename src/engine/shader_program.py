7afbe2e8b3530d1757dbffab3db38a012633c462
severusDude <fajarsaladin05@gmail.com>
2026-04-25 22:44:31 +0700

    fix(shaders): incorrect shaders dir path

--- src/engine/shader_program.py
+++ src/engine/shader_program.py
@@ -9,13 +9,13 @@ Programs:
   - fish          : animated fish with simple color stripe
 """
 
-import os
+from pathlib import Path
 
 
 class ShaderProgram:
     def __init__(self, ctx):
         self.ctx = ctx
-        shader_dir = os.path.join(os.path.dirname(__file__), 'shaders')
+        shader_dir = Path(__file__).resolve().parents[2] / "shaders"
         self.programs = {
             'phong_color': self._load(shader_dir, 'phong_color'),
             'bubble':      self._load(shader_dir, 'bubble'),
